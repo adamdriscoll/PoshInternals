095d0919fafe47e2cae0c1200b9ab5e2491db9dd
Adam Driscoll <adam.r.driscoll@gmail.com>
2013-01-29 19:28:04 -0600

    Renamed Watch-Process to Out-MiniDump

--- Procdump.ps1
+++ Procdump.ps1
@@ -1,25 +1,43 @@
-﻿function Watch-Process
+﻿function Out-MiniDump
 {
     [CmdletBinding()]
     param(
-    [Parameter(ValueFromPipeline=$True)]
-    [System.Diagnostics.Process]$Process
+    [Parameter(ValueFromPipeline=$True, Mandatory=$true)]
+    [System.Diagnostics.Process]$Process,
+    [Parameter()]
+    [string]$Path,
+    [Parameter()]
+    [Switch]$Full
     )
 
     Process
     {
-        $DumpFile = Join-Path ([Environment]::CurrentDirectory) "$($Process.ID).dmp"
+        if ([String]::IsNullOrEmpty($Path))
+        {
+            $Path = Join-Path ([Environment]::CurrentDirectory) "$($Process.ID).dmp"
+        }
 
-        $FileName = [PoshInternals.NativeMethods]::CreateFile($DumpFile, [System.IO.FileAccess]::Write, [System.IO.FileShare]::None, [IntPtr]::Zero, [System.IO.FileMode]::CreateNew, 0, [IntPtr]::Zero)
+        if ($Full)
+        {
+            $DumpType = [PoshInternals.MINIDUMP_TYPE]::MiniDumpWithFullMemory
+        }
+        else
+        {
+            $DumpType = [PoshInternals.MINIDUMP_TYPE]::MiniDumpNormal
+        }
+
+        $FileName = [PoshInternals.NativeMethods]::CreateFile($Path, [System.IO.FileAccess]::Write, [System.IO.FileShare]::None, [IntPtr]::Zero, [System.IO.FileMode]::CreateNew, 0, [IntPtr]::Zero)
         if ($FileName.IsInvalid)
         {
             throw New-Object System.ComponentModel.Win32Exception
         }
 
-        if (-not [PoshInternals.NativeMethods]::MiniDumpWriteDump($Process.Handle, $Process.Id, $FileName, [PoshInternals.MINIDUMP_TYPE]::MiniDumpWithFullMemory, [IntPtr]::Zero, [IntPtr]::Zero, [IntPtr]::Zero))
+        if (-not [PoshInternals.NativeMethods]::MiniDumpWriteDump($Process.Handle, $Process.Id, $FileName, $DumpType, [IntPtr]::Zero, [IntPtr]::Zero, [IntPtr]::Zero))
         {
             throw New-Object System.ComponentModel.Win32Exception
         }
+        
+
     }
 }
 
